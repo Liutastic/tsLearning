03b7b612513bd580595ce05b1de1c5e91ab376bc
Liutastic <githubliutastic@163.com>
2021-06-26 15:03:47 +0800

    严格检查配置

--- autoTs/src/app.ts
+++ autoTs/src/app.ts
@@ -1,2 +1,2 @@
-import { a } from './m'
-console.log(a)
+// import { a } from './m'
+console.log('sds')
--- autoTs/src/m.ts
+++ autoTs/src/m.ts
@@ -1,1 +1,2 @@
-export const a: number = 2333
+// export const a: number = 2333
+console.log('231231231232')
